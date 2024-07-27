a664c3d2bf45ba815d3b4ad9d388477f8ce6d65b
joseph0926 <joseph0926@naver.com>
2024-07-28 08:33:17 +0900

    chore: [1] 프로젝트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'joseph0926`s blog',
+  description: '프론트엔드 개발자 김영훈의 개인 블로그입니다.',
+  icons: {
+    icon: '/logo.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
