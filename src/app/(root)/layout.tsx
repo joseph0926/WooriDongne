e0e47288278c93158266bbf1b9a555f39a1354c1
joseph0926 <joseph0926@naver.com>
2024-08-30 10:23:47 +0900

    refactor: [6] 디자인 변경

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,3 @@
-import { Navbar } from '@/components/navbar';
-
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="py-7 pl-[8rem] pr-[3rem] lg:px-[6rem]">
-      <Navbar />
-      {children}
-    </div>
-  );
+  return <div className="">{children}</div>;
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,6 @@
-import { AllPostList } from '@/components/post/all-post-list';
-import { RecentPostList } from '@/components/post/recent-post-list';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-10">
-      <RecentPostList />
-      <AllPostList />
-    </div>
+    <div></div>
   );
 }
