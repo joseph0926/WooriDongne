8e15a4a0ec5b8298e5327834f87a760afd980b65
joseph0926 <joseph0926@naver.com>
2024-09-22 08:50:25 +0900

    chore: metadata 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,9 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'joseph0926`s Blog',
-  description: '프론트엔드 개발자 김영훈의 개인 블로그입니다.',
+  title: 'WooriDongne',
+  description:
+    '우리동네는 지역 기반 커뮤니티 플랫폼으로, 이웃 간의 소통을 촉진하고 지역 사회의 활성화에 기여하는 것을 목표로 합니다. 사용자는 자신의 거주지를 기반으로 한 커뮤니티에 참여하여 이웃들과 정보를 공유하고, 중고 거래, 이벤트 참여 등 다양한 활동을 즐길 수 있습니다.',
   icons: {
     icon: '/logo.svg',
   },
