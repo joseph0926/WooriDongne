fb8b86c03525ec7fd51c17e1be7d4ea2bbf311dc
joseph0926 <joseph0926@naver.com>
2024-09-22 12:58:04 +0900

    feature: [1] prisma 정의 / bcryptjs 추가

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = db;
+}
