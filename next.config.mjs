402685cc4585888572852fbde4f1824637ccf64b
joseph0926 <joseph0926@naver.com>
2024-09-30 07:36:30 +0900

    chore: [12] 임시 이미지 url 등록

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // TODO: 임시 이미지 - 이후 삭제
+      {
+        hostname: 'assets.aceternity.com',
+        protocol: 'https',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
