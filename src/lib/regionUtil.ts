ea02ff45ef29e2c55cd724111922394197a04c35
joseph0926 <joseph0926@naver.com>
2024-09-29 17:29:15 +0900

    refactor: [11] region 유틸함수들 주석 추가

--- src/lib/regionUtil.ts
+++ src/lib/regionUtil.ts
@@ -1,5 +1,10 @@
 import { AddressType, RegionalInfo } from '@/types/profile.type';
 
+/**
+ * daum-postcode 데이터에서 필요한 데이터만 추출하는 함수
+ * @param data
+ * @returns { city, district, neighborhood }
+ */
 export function parseAddress(data: AddressType): RegionalInfo {
   const { sido, sigungu, bname } = data;
 
@@ -10,6 +15,13 @@ export function parseAddress(data: AddressType): RegionalInfo {
   return { city, district, neighborhood };
 }
 
+/**
+ * address 대표 네임 생성 함수
+ * @param city
+ * @param district
+ * @param neighborhood
+ * @returns `${district} ${neighborhood}`
+ */
 export function createRegionName(city: string, district: string, neighborhood: string): string {
   return `${district} ${neighborhood}`;
 }
